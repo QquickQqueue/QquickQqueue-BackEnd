69b9a532a02f8c0f6c0d9eb2a1c63bbe721d2f98
been1118 <dsin1118@kakao.com>
2023-12-13 23:16:48 +0900

    first commit

---
+++ src/main/java/com/example/qquickqqueue/QquickQqueueApplication.java
@@ -0,0 +1,13 @@
+package com.example.qquickqqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QquickQqueueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QquickQqueueApplication.class, args);
+    }
+
+}
