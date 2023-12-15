8fb3892a363566b07f1bedf7a97708b4e2cb5f23
been1118 <dsin1118@kakao.com>
2023-12-16 02:33:17 +0900

    feat : 뮤지컬 전체 조회 (메인페이지) 기능 구현

---
+++ src/main/java/com/example/qquickqqueue/domain/musical/controller/MusicalController.java
@@ -0,0 +1,25 @@
+package com.example.qquickqqueue.domain.musical.controller;
+
+import com.example.qquickqqueue.domain.musical.service.MusicalService;
+import com.example.qquickqqueue.util.Message;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class MusicalController {
+
+    private final MusicalService musicalService;
+
+    @GetMapping("/musicals")
+    public ResponseEntity<Message> readMusicals(@PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.ASC) Pageable pageable) {
+        return musicalService.readMusicals(pageable);
+    }
+}
---
+++ src/main/java/com/example/qquickqqueue/domain/musical/service/MusicalService.java
@@ -0,0 +1,28 @@
+package com.example.qquickqqueue.domain.musical.service;
+
+import com.example.qquickqqueue.domain.musical.dto.MusicalResponseDto;
+import com.example.qquickqqueue.domain.musical.repository.MusicalRepository;
+import com.example.qquickqqueue.util.Message;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MusicalService {
+    private final MusicalRepository musicalRepository;
+    public ResponseEntity<Message> readMusicals(Pageable pageable) {
+        return new ResponseEntity<>(new Message("조회 성공", musicalRepository.findAll(pageable)
+                .map(musical -> MusicalResponseDto.builder()
+                        .title(musical.getTitle())
+                        .thumbnailUrl(musical.getThumbnailUrl())
+                        .rating(musical.getRating())
+                        .description(musical.getDescription())
+                        .startDate(musical.getStartDate())
+                        .endDate(musical.getEndDate())
+                        .runningTime(musical.getRunningTime())
+                        .build())), HttpStatus.OK);
+    }
+}
