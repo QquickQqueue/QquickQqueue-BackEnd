ab01726e0c3d968f464b2c4abea51bec9434230c
been1118 <dsin1118@kakao.com>
2023-12-16 02:28:49 +0900

    fix : 406 error 해결

--- src/main/java/com/example/qquickqqueue/util/Message.java
+++ src/main/java/com/example/qquickqqueue/util/Message.java
@@ -1,7 +1,9 @@
 package com.example.qquickqqueue.util;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
 @AllArgsConstructor
 public class Message {
     private String message;
