733fdc12dea620a5b202020f207415f6a59d536e
been1118 <dsin1118@kakao.com>
2023-12-22 21:03:01 +0900

    feat : 뮤지컬 검색기능 API 구현

--- src/main/java/com/example/qquickqqueue/domain/musical/repository/MusicalRepository.java
+++ src/main/java/com/example/qquickqqueue/domain/musical/repository/MusicalRepository.java
@@ -1,9 +1,12 @@
 package com.example.qquickqqueue.domain.musical.repository;
 
 import com.example.qquickqqueue.domain.musical.entity.Musical;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MusicalRepository extends JpaRepository<Musical, Long> {
+    Page<Musical> findAllByTitleContaining(String title, Pageable pageable);
 }
