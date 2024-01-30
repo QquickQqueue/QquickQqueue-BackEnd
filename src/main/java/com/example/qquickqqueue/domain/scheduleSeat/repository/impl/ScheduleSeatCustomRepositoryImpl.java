a89d19b50b06988f4a1d16f01f55f136389f40c6
silversheep26 <awindya@naver.com>
2024-01-31 01:16:12 +0900

    refactor : 뮤지컬좌석 조회 리펙토링(querydsl)
    Co-authored-by: been1118 <dsin1118@kakao.com>
    
    Co-authored-by: LEOJI <kjh11-07@hanmail.net>

---
+++ src/main/java/com/example/qquickqqueue/domain/scheduleSeat/repository/impl/ScheduleSeatCustomRepositoryImpl.java
@@ -0,0 +1,37 @@
+package com.example.qquickqqueue.domain.scheduleSeat.repository.impl;
+
+import com.example.qquickqqueue.domain.scheduleSeat.entity.QScheduleSeat;
+import com.example.qquickqqueue.domain.scheduleSeat.entity.ScheduleSeat;
+import com.example.qquickqqueue.domain.scheduleSeat.repository.ScheduleSeatCustomRepository;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class ScheduleSeatCustomRepositoryImpl implements ScheduleSeatCustomRepository {
+	private final JPAQueryFactory query;
+	private final QScheduleSeat qScheduleSeat = QScheduleSeat.scheduleSeat;
+
+	@Override
+	public List<ScheduleSeat> findAllBySchedule_IdAndIsReserved(Long scheduleId, boolean isReserved) {
+		return query.selectFrom(qScheduleSeat)
+			.join(qScheduleSeat.schedule).fetchJoin()
+			.join(qScheduleSeat.seat).fetchJoin()
+			.join(qScheduleSeat.seatGrade).fetchJoin()
+			.where(qScheduleSeat.schedule.id.eq(scheduleId)
+				.and(qScheduleSeat.isReserved.eq(isReserved)))
+			.fetch();
+	}
+
+	@Override
+	public List<ScheduleSeat> findAllBySchedule_Id(Long scheduleId) {
+		return query.selectFrom(qScheduleSeat)
+			.join(qScheduleSeat.seatGrade).fetchJoin()
+			.join(qScheduleSeat.schedule).fetchJoin()
+			.join(qScheduleSeat.seat).fetchJoin()
+			.where(qScheduleSeat.schedule.id.eq(scheduleId))
+			.fetch();
+	}
+}
