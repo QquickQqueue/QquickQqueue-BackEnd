b1bb7d8e516c2b2c1538348693e21870e95fb898
been1118 <dsin1118@kakao.com>
2024-01-31 21:50:30 +0900

    refactor : ScheduleSeatRepository.findById() 비관적 락 적용
    Co-authored-by: silversheep26 <awindya@naver.com>
    Co-authored-by: LEOJI <kjh11-07@hanmail.net>

--- src/main/java/com/example/qquickqqueue/domain/scheduleSeat/repository/ScheduleSeatRepository.java
+++ src/main/java/com/example/qquickqqueue/domain/scheduleSeat/repository/ScheduleSeatRepository.java
@@ -1,10 +1,18 @@
 package com.example.qquickqqueue.domain.scheduleSeat.repository;
 
 import com.example.qquickqqueue.domain.scheduleSeat.entity.ScheduleSeat;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface ScheduleSeatRepository extends JpaRepository<ScheduleSeat, Long>, ScheduleSeatCustomRepository {
     ScheduleSeat findByScheduleIdAndSeatId(Long scheduleId, Long seatId);
+
+    @Override
+    @Lock(value = LockModeType.PESSIMISTIC_WRITE)
+    Optional<ScheduleSeat> findById(Long aLong);
 }
