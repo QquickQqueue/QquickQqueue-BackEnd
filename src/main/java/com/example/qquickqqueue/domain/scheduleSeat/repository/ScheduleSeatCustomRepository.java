faab0867f0580453f4996782ca62797734d56b90
been1118 <dsin1118@kakao.com>
2024-03-01 01:12:37 +0900

    fix : 쿼리에 for update가 추가되면서 읽기 전용엔 안된다고 하는 에러 픽스

--- src/main/java/com/example/qquickqqueue/domain/scheduleSeat/repository/ScheduleSeatCustomRepository.java
+++ src/main/java/com/example/qquickqqueue/domain/scheduleSeat/repository/ScheduleSeatCustomRepository.java
@@ -2,9 +2,12 @@ package com.example.qquickqqueue.domain.scheduleSeat.repository;
 
 import com.example.qquickqqueue.domain.scheduleSeat.entity.ScheduleSeat;
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.repository.query.Param;
 
 public interface ScheduleSeatCustomRepository {
 	List<ScheduleSeat> findAllBySchedule_IdAndIsReserved(@Param("scheduleId") Long scheduleId, @Param("isReserved") boolean isReserved);
 	List<ScheduleSeat> findAllBySchedule_Id(@Param("scheduleId") Long scheduleId);
+	Optional<ScheduleSeat> findById(Long id);
 }
