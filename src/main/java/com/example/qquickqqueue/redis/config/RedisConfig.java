b38951d1ffcc8a4b6c13c5edcf5d2fc114a5a331
been1118 <dsin1118@kakao.com>
2024-03-13 22:31:27 +0900

    chore : config class 테스트에서 제외

--- src/main/java/com/example/qquickqqueue/redis/config/RedisConfig.java
+++ src/main/java/com/example/qquickqqueue/redis/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.example.qquickqqueue.redis.config;
 
+import lombok.Generated;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +12,7 @@ import org.springframework.data.redis.repository.configuration.EnableRedisReposi
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+@Generated
 @Configuration
 @RequiredArgsConstructor
 @EnableRedisRepositories
@@ -23,8 +25,8 @@ public class RedisConfig {
 	}
 
 	@Bean
-	public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
-		RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+	public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory connectionFactory) {
+		RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
 		redisTemplate.setConnectionFactory(connectionFactory);
 		redisTemplate.setKeySerializer(new StringRedisSerializer());
 		redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(String.class));
